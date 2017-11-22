9febb63e22f600ad64b265bf0ae9528440494d4a
L00n3y <aiz3nsama1@gmail.com>
2017-11-22 23:55:23 +0100

    Verbetering
    bugs fixed

--- InteressanteGetallen.py
+++ InteressanteGetallen.py
@@ -4,30 +4,32 @@ Studentnummer:  S1104522
 Specialisatie:  IFICT
 """
 
-# vraagt de input aan de gebruiker hoeveel getallen hij wil als deze maar onder de 50 is
+#Vraagt gebruiker om invoer testgevallen
 Waarde1 = int(input("Vul het aantal test getallen in lager dan 50"))
 Cijferlijst = []
-
-"""
-De waarde wordt de lengte van de array
-Hierdoor kun je de testgetallen invullen
-"""
-
+Lijst = []
+c = 0
 a = 0
 
+#zolang a niet gelijk is aan waarde1, voegt de invoer van de gebruiker aan Cijferlijst toe. en zet a op 1 enzovoort.
 while a != Waarde1:
     Cijferlijst.append(int(input("Vul een getal in tussen 0 en 100")))
     a += 1
 
-#Haal de testgetallen door de functie
 
+#een loop dat stopt tot het bij aantal nummers in Cijferlijst is. vervolgens zet getallen gelijk aan b. daarna zet reken gelijk aan getallen.
+#de while not loop maakt een variable x aan van formaat int, vervolgens maken wij van getallen een string.
+#en met de python functie sum tellen wij de x en getallen bij elkaar op, als dit gelijk is aan variable reken
+#voert hij getallen + reken uit.
 for b in Cijferlijst:
     getallen = b
-    reken = getallen
 
-    while not(sum([int(x) for x in str(getallen)]) == reken):
-        getallen += reken
+    while not(sum([int(x) for x in str(getallen)]) == b):
+        getallen += b
 
-print(Cijferlijst[0])
-print(getallen)
+    Lijst.append(getallen)
+    c += 1
+
+for i in range(0, c):
+    print(Lijst[i])
 
